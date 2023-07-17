9a68546d96f141e0a265c1bb7649370145f51397
Dhruva Hegde <53150130+Intelectron6@users.noreply.github.com>
2023-07-17 14:15:47 +0530

    Update wrt_ptr_full.v

--- Asynchronous_FIFO/wrt_ptr_full.v
+++ Asynchronous_FIFO/wrt_ptr_full.v
@@ -1,11 +1,11 @@
 module wrt_ptr_full (output reg full,
-					 output [2:0] wrt_addr,
-					 output reg [3:0] wrt_ptr,
-					 input [3:0] wq2_rd_ptr,
-					 input wrt_en, wrt_clk, wrt_rst_n);
+		     output [5:0] wrt_addr,
+		     output reg [6:0] wrt_ptr,
+		     input [6:0] wq2_rd_ptr,
+		     input wrt_en, wrt_clk, wrt_rst_n);
 	
-	reg [3:0] wrt_bin;
-	wire [3:0] wrt_gray_next, wrt_bin_next;
+	reg [6:0] wrt_bin;
+	wire [6:0] wrt_gray_next, wrt_bin_next;
 	wire full_val;
 	
 	// Gray Style 2 pointer (both gray code and binary code registers are present)
@@ -19,13 +19,13 @@ module wrt_ptr_full (output reg full,
 	assign wrt_gray_next = (wrt_bin_next>>1) ^ wrt_bin_next;
 	
 	// Memory write-address pointer (binary is used to address memory)
-	assign wrt_addr = wrt_bin[2:0];
+	assign wrt_addr = wrt_bin[5:0];
 	
 	// Simplified version of the three necessary full-tests:
-	// assign full_val=((wrt_gray_next[3] != wq2_rd_ptr[3]) &&
-	//					(wrt_gray_next[2] != wq2_rd_ptr[2]) && 
-	//   				(wrt_gray_next[1:0] == wq2_rd_ptr[1:0]));
-	assign full_val = (wrt_gray_next=={~wq2_rd_ptr[3:2], wq2_rd_ptr[1:0]});
+	// assign full_val=((wrt_gray_next[6] != wq2_rd_ptr[6]) &&
+	//		    (wrt_gray_next[5] != wq2_rd_ptr[5]) && 
+	//   		    (wrt_gray_next[4:0] == wq2_rd_ptr[4:0]));
+	assign full_val = (wrt_gray_next=={~wq2_rd_ptr[6:5], wq2_rd_ptr[4:0]});
 	
 	always @(posedge wrt_clk or negedge wrt_rst_n)
 		if (!wrt_rst_n) 
