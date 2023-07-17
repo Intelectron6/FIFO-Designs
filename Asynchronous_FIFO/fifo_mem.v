4e7bc54fcc94fd33e27c5850d455b66c6ae212e0
Dhruva Hegde <53150130+Intelectron6@users.noreply.github.com>
2023-07-17 14:13:18 +0530

    Update fifo_mem.v

--- Asynchronous_FIFO/fifo_mem.v
+++ Asynchronous_FIFO/fifo_mem.v
@@ -1,10 +1,10 @@
 module fifo_mem (output [15:0] rd_data,
-				 input [15:0] wrt_data,
-				 input [2:0] wrt_addr, rd_addr,
-				 input wrt_en, full, wrt_clk);
+		 input [15:0] wrt_data,
+		 input [2:0] wrt_addr, rd_addr,
+		 input wrt_en, full, wrt_clk);
 
 	// RTL Verilog memory model
-	reg [15:0] mem [0:7];
+	reg [15:0] mem [0:63];
 	
 	assign rd_data = mem [rd_addr];
 	
