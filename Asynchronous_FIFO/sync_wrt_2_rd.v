1d940079d24ca0d7562713cea8f29d2de3467431
Dhruva Hegde <53150130+Intelectron6@users.noreply.github.com>
2023-07-17 14:16:22 +0530

    Update sync_wrt_2_rd.v

--- Asynchronous_FIFO/sync_wrt_2_rd.v
+++ Asynchronous_FIFO/sync_wrt_2_rd.v
@@ -1,8 +1,8 @@
-module sync_wtr_2_rd (output reg [3:0] rq2_wrt_ptr,
-				      input [3:0] wrt_ptr,
-					  input rd_clk, rd_rst_n);
+module sync_wtr_2_rd (output reg [6:0] rq2_wrt_ptr,
+		      input [6:0] wrt_ptr,
+		      input rd_clk, rd_rst_n);
 				 
-	reg [3:0] temp_ptr;
+	reg [6:0] temp_ptr;
 	
 	// 2 flop synchronizer for write point with respect to read clock
 	always @(posedge rd_clk or negedge rd_rst_n)
